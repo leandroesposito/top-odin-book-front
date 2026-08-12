9588b15e9ce4e09930fa21477796d0f2e5e9382a
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-08-11 21:17:58 -0300

    Redirect if root route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 import "./App.css";
 import Header from "./components/parts/Header";
+import { isLogedIn } from "./session/sessionManager";
 
 function App() {
+  const location = useLocation();
+
+  if (location.pathname === "/") {
+    console.log(location);
+    if (isLogedIn()) {
+      return <Navigate to={"/feed"} />;
+    } else {
+      return <Navigate to={"/log-in"} />;
+    }
+  }
+
   return (
     <>
       <Header />
