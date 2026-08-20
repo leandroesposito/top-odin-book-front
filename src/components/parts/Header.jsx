7fd54c3909afb084b61508cca008ac328241e477
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-08-20 15:47:50 -0300

    Implement dark mode

--- src/components/parts/Header.jsx
+++ src/components/parts/Header.jsx
@@ -9,10 +9,37 @@ import {
 import "./Header.css";
 import Avatar from "./Avatar";
 import useFetch from "../../hooks/useFetch";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Loading from "./Loading/Loading";
+import { Moon, Sun } from "lucide-react";
 
 function Header() {
+  function displayDarkMode(state) {
+    if (state) {
+      document.querySelector("body").classList.add("dark-mode");
+      localStorage.setItem("dark-mode", "1");
+    } else {
+      document.querySelector("body").classList.remove("dark-mode");
+      localStorage.setItem("dark-mode", "");
+    }
+  }
+
+  function getSystemDarkMode() {
+    const storageMode = localStorage.getItem("dark-mode");
+    const mq = window.matchMedia("(prefers-color-scheme: dark)");
+    let value = false;
+
+    if (storageMode !== null) {
+      value = Boolean(storageMode);
+    } else if (mq.matches) {
+      value = true;
+    }
+
+    displayDarkMode(value);
+    return value;
+  }
+
+  const [isDarkMode, setIsDarkMode] = useState(getSystemDarkMode);
   const { loading, data, success, makeRequest } = useFetch();
 
   useEffect(() => {
@@ -35,8 +62,15 @@ function Header() {
     makeRequest("/auth/log-in/guest", "POST");
   }
 
+  function onDarkmodeChange(event) {
+    const target = event.target;
+    const checked = target.checked;
+    setIsDarkMode(checked);
+    displayDarkMode(checked);
+  }
+
   return (
-    <header className="header">
+    <header className={`header ${isDarkMode ? "dark-mode" : ""}`}>
       <div className="left">
         <div className="logo">
           <Link to={"/"}>
@@ -46,6 +80,19 @@ function Header() {
       </div>
       <div className="right">
         <div className="buttons">
+          <label htmlFor="dark-mode-switch" className="dark-mode-switch-label">
+            <input
+              type="checkbox"
+              name="dark-mode-switch"
+              id="dark-mode-switch"
+              onChange={onDarkmodeChange}
+              checked={isDarkMode}
+              aria-label="Color theme switch"
+            />
+            <div className="dark-mode-icon">
+              {isDarkMode ? <Moon /> : <Sun />}
+            </div>
+          </label>
           {isLogedIn() ? (
             <>
               <Link to={`/profile/${getUserId()}`} className="button">
