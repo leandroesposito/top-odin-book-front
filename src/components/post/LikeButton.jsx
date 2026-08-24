d92f4f632e195d738a6d15d31bcadb7d3c35dd30
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-08-24 19:25:31 -0300

    Replace unlike post button icon with color

--- src/components/post/LikeButton.jsx
+++ src/components/post/LikeButton.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import { isLogedIn } from "../../session/sessionManager";
-import { Heart, HeartOff } from "lucide-react";
+import { Heart } from "lucide-react";
 
 function LikeButton({ post }) {
   const { loading, data, errors, makeRequest } = useFetch();
@@ -35,11 +35,11 @@ function LikeButton({ post }) {
 
   return (
     <button
-      className={`${liked ? "dislike" : "like"}-post-button button`}
+      className={`like-post-button button ${liked ? "dislike-post-button" : ""}`}
       disabled={loading}
       onClick={onLikeClick}
     >
-      {liked ? <HeartOff /> : <Heart />} (
+      <Heart /> (
       {post.likes_count + (liked !== post.liked ? (liked ? 1 : -1) : 0)})
     </button>
   );
